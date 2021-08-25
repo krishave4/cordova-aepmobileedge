0a5666ffbf925324ddb0abd034924443d7862bee
krishave4 <89221673+krishave4@users.noreply.github.com>
2021-08-26 00:46:02 +0530

    Update AEPMobileEdge.js

--- www/AEPMobileEdge.js
+++ www/AEPMobileEdge.js
@@ -1,23 +1,22 @@
-cordova.define("cordova-aepmobileedge.cordova-aepmobileedge", function(require, exports, module) {
-    var AEPMobileEdge = (function() {
-        var AEPMobileEdge = (typeof exports !== 'undefined') && exports || {};
-        var exec = cordova.require('cordova/exec'); // eslint-disable-line no-undef
-    
-        var PLUGIN_NAME = "AEPMobileEdge_Cordova";
-    
-        // ===========================================================================
-        // public objects
-        // ===========================================================================
+var AEPMobileEdge = (function() {
 
-        AEPMobileEdge.sendEvent = function(sdkEvent, success, fail) {
-            var FUNCTION_NAME = "sendEvent";
-    
-            return exec(success, fail, PLUGIN_NAME, FUNCTION_NAME, [sdkEvent]);
-        };
+    var exec = cordova.require('cordova/exec');
+    var AEPMobileEdge = (typeof exports !== 'undefined') && exports || {};
 
-        return AEPMobileEdge;
+    var PLUGIN_NAME = "AEPMobileEdge_Cordova";
 
-    }());    
-    module.exports = AEPMobileEdge;
+    // ===========================================================================
+    // public objects
+    // ===========================================================================
 
-});
+    AEPMobileEdge.sendEvent = function(sdkEvent, success, fail) {
+        var FUNCTION_NAME = "sendEvent";
+
+        return exec(success, fail, PLUGIN_NAME, FUNCTION_NAME, [sdkEvent]);
+    };
+
+    return AEPMobileEdge;
+
+}());
+
+export default AEPMobileEdge;
